73e4f907a31fa168b68c15b3a71ce1b1a480d974
brianmwevi <mwevibrian@gmail.com>
2022-04-19 03:47:49 +0300

    add user and repo interface

---
+++ src/app/models/Repo.ts
@@ -0,0 +1,11 @@
+export interface Repo {
+  name: string;
+  language: string;
+  created_at: string;
+  licence: string;
+  // description: string;
+  updated_at: string;
+  forks_count: number;
+  watchers_count: number;
+  html_url: string;
+}
