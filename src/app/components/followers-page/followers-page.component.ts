8647adb156fa87faaaeaedff325d2562cdb71859
brianmwevi <mwevibrian@gmail.com>
2022-04-19 03:21:02 +0300

    ft: add followers page

---
+++ src/app/components/followers-page/followers-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-followers-page',
+  templateUrl: './followers-page.component.html',
+  styleUrls: ['./followers-page.component.css'],
+})
+export class FollowersPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
